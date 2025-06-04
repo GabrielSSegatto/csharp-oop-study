79d9bf6fe635ff5b5536075a162cfba7d1196f6a
Gabriel Segatto <gabriels080404@gmail.com>
2025-06-03 22:20:57 -0300

    Primeiro commit de estudos de POO em C#, Programa pra calcular área de triangulos

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using aulas;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+
+
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            x.A = double.Parse(Console.ReadLine());
+            x.B = double.Parse(Console.ReadLine());
+            x.C = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            y.A = double.Parse(Console.ReadLine());
+            y.B = double.Parse(Console.ReadLine());
+            y.C = double.Parse(Console.ReadLine());
+
+            double areaX = x.Area();
+            double areaY = y.Area();
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            } 
+        } 
+    }
+}
